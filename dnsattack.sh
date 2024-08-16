b8c825878a44feff0e16423e4ee458a784a6955c
jacksoncastilho <studyjacksoncastilho@gmail.com>
2024-08-15 20:35:33 -0400

    Changed

--- dnsattack.sh
+++ dnsattack.sh
@@ -8,10 +8,12 @@ echo -e "\n"
 
 _dnsZoneTransfer() {
     read -p "domain: " domain
+
     for ns in $(host -t ns $domain | cut -d " " -f 4); do
         echo -e "\n######## $ns ########\n"
-        host -a $domain $ns
+        host -t AXFR $domain $ns
     done
+
     exit 0
 }
 
@@ -26,10 +28,18 @@ _dnsBrute() {
 }
 
 _dnsReverse() {
-    read -p "subnet prefix: " subnet_prefix
+    read -p "target ip: " target
+    
+    whois $target | grep "inetnum" 
+    
+    subnet_prefix=$(echo $target | cut -d . -f 1,2,3)
+    echo -e "subnet prefix: " $subnet_prefix 
+    
     read -p "range first: " first
     read -p "range last: " last
-
+    
+    echo -e "\n"
+    
     for host_id in $(seq $first $last); do
         host -t ptr $subnet_prefix.$host_id
     done
@@ -37,7 +47,7 @@ _dnsReverse() {
     exit 0
 }
 
-echo "Choice an attack:"
+echo Choice an attack:
 select choice in dnszonetransfer dnsbrute dnsreverse; do
     case $choice in
         dnszonetransfer ) _dnsZoneTransfer ;;
